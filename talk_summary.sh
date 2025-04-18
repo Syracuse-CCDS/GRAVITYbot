86a12471086bc3ee76faa3639e70d3b5ce2d8631
Jonathan R Stromer-Galley <jrstrome@syr.edu>
2025-04-18 09:50:21 -0400

    Try to fix crontab compatibility

--- talk_summary.sh
+++ talk_summary.sh
@@ -1,1 +1,4 @@
-docker run -it --mount src=/opt/GRAVITYbot,target=/var/task,type=bind gravitybot python ./_src/__talk_summary_main__.py >> /opt/GRAVITYbot/jobs.log 2>&1
+#!/usr/bin/env bash
+
+#docker run -it --mount src=/opt/GRAVITYbot,target=/var/task,type=bind gravitybot python ./_src/__talk_summary_main__.py >> /opt/GRAVITYbot/jobs.log 2>&1
+docker run --mount src=/opt/GRAVITYbot,target=/var/task,type=bind gravitybot python ./_src/__talk_summary_main__.py >> /opt/GRAVITYbot/jobs.log 2>&1
